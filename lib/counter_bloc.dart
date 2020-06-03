acefce97ccb3227713c2f4cfea11c40407eda454
jmayta1984 <Monica$2017>
2020-06-02 23:57:48 -0500

    Ejemplo usando el patrón bloc

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:flutter_bloc_demo/counter_event.dart';
+
+class CounterBloc {
+  int _counter = 0;
+
+  final _counterStateController = StreamController<int>();
+
+  StreamSink<int> get _inCounter => _counterStateController.sink;
+  Stream<int> get outCounter => _counterStateController.stream;
+
+  final _counterEventController = StreamController<CounterEvent>();
+  Sink<CounterEvent> get counterEventSink => _counterEventController.sink;
+
+  CounterBloc() {
+    _counterEventController.stream.listen(_mapEventToState);
+  }
+
+  void _mapEventToState(CounterEvent event) {
+    if (event is IncrementEvent)
+      _counter++;
+    else
+      _counter--;
+
+    _inCounter.add(_counter);
+  }
+}
